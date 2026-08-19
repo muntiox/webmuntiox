75401874971f84769b74c0dce9617c1095267cb0
Itxaso Muntión <tu@email.com>
2026-08-19 17:03:58 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,17 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
 import contentCollections from '@content-collections/vite'
 
 export default defineConfig({
   plugins: [
+    TanStackRouterVite(),
     contentCollections(),
-    TanStackRouterVite({
-      routesDirectory: './src/routes',
-      generatedRouteTree: './src/routeTree.gen.ts',
-    }),
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    viteReact(),
+    react(),
   ],
-  build: {
-    outDir: 'dist',
+  server: {
+    watch: {
+      usePolling: true,
+    },
   },
 })
